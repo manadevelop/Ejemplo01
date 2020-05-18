c82e4c45b3dc20496261952cf681ca411c58f4e8
Marco Antonio <masoft.system@hotmail.com>
2020-05-18 16:33:03 -0500

    Subiendo el cambio

--- app/src/main/java/pe/gob/reniec/MainActivity.java
+++ app/src/main/java/pe/gob/reniec/MainActivity.java
@@ -3,6 +3,7 @@ package pe.gob.reniec;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,5 +11,8 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        TextView tvSaludo = (TextView) findViewById(R.id.saludo);
+        tvSaludo.setText("Estamos realizando cambios");
     }
 }
